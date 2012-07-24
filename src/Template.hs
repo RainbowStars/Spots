75b5b95e22caea6701f69a3ad82e13a58c89090b
Stars <precious.metals.and.rose.petals@gmail.com>
2012-07-24 01:32:37 -0400

    general rearranging and cleanup

--- src/Template.hs
+++ src/Template.hs
@@ -3,6 +3,9 @@ module Template where
 
 import Types
 
+import Template.Header
+import Template.Thumbs
+
 import Prelude hiding (head, id, div)
 import Data.List hiding (head)
 import System.Directory
@@ -20,40 +23,12 @@ index conf = html (do
         H.title "some kind of title should go here I guess"
         link ! href "Spots.css" ! rel "stylesheet")
     body (do
-        spotsHeader
-        thumbs conf))
+        toHtml . unsafePerformIO $ (thumbs conf (rootDir conf))))   -- FIXME unsafe IO
 
-spotsHeader :: Html
-spotsHeader = do
-    div ! id "header" $ do
-        p "hello"
-
-thumbs :: SpotsConf -> Html
-thumbs conf = toHtml (unsafePerformIO (do       -- generate list of thumbs  -- FIXME unsafe IO
-    fileList <- getDirectoryContents "."        -- we should have cd'd to the rootDir in the main function
-    return (map (thumb conf)
-        (filter (\file -> or [
-            (isImage file),
-            (and [isDirectory file,
-                (not (isSpecial conf file))])])
-        fileList)))) where
-        isDirectory file = unsafePerformIO (doesDirectoryExist file)    -- FIXME unsafe IO
-        isImage file = or (map (\extension -> isSuffixOf extension file) imageExtensions)
-        isSpecial conf file = or (map (\file' -> file == file') [
-            ".",
-            "..",
-            ".DS_Store",
-            (thumbsDir conf)])
-
-thumb :: SpotsConf -> FilePath -> Html
-thumb conf file = do        -- html for a single thumbnail box containing thumbnail and caption
-    div ! id "thumb" $ do
-        a ! href file' $ do
-            img ! src thumbPath ! alt file'
-        p (string file) where
-        file' = stringValue file
-        thumbPath = stringValue (((thumbsDir conf) ++ "/") ++ file'') where
-            file'' = case isDirectory of
-                True -> file ++ "/cover.jpg"
-                False -> file
-            isDirectory = unsafePerformIO (doesDirectoryExist file) -- FIXME unsafe IO
+thumbs :: SpotsConf -> FilePath -> IO [Html]
+thumbs conf path = do
+    fileList <- getDirectoryContents path
+    return (map (thumb conf) (filter isValid fileList)) where
+            isValid file = or [isImage file,
+                        and [unsafePerformIO (isDirectory file),    -- FIXME unsafe IO
+                       (not (isSpecial conf file))]]
